69f66ec6437254ae8ab9dcb545156f6295e15752
Ursin Filli <git@ursinn.dev>
2020-05-02 14:10:37 +0200

    Example 3

---
+++ example3.d
@@ -0,0 +1,31 @@
+import std.stdio;
+import std.conv;
+import std.math;
+import std.random;
+import std.datetime;
+import std.algorithm;
+import std.array;
+import std.string;
+import std.format;
+import std.typecons : No;
+import std.typecons;
+import std.range : chain;
+import std.file;
+import core.thread;
+import std.parallelism;
+import std.concurrency;
+import std.range;
+
+void main() {
+    int cInt = 15;
+    short cShort = cast(short) cInt;
+    short cShort2 = cast(short) 32768;
+    writeln("Short cast : ", cShort2);
+
+    auto sInt = to!string(10);
+    writeln("Type : ", typeof(sInt).stringof);
+
+    auto iStr = to!int("10");
+    writeln("Type : ", typeof(iStr).stringof);
+
+}
