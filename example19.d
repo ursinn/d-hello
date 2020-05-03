f52847b0a0ed72aa47d126339bb7e81eb4b6f4c2
Ursin Filli <git@ursinn.dev>
2020-05-03 21:59:29 +0200

    Example 19

---
+++ example19.d
@@ -0,0 +1,33 @@
+import std.stdio;
+import std.conv;
+import std.math;
+import std.random;
+import std.datetime;
+import std.algorithm;
+import std.array;
+import std.string;
+import std.format;
+import std.typecons : No;
+import std.typecons;
+import std.range : chain;
+import std.file;
+import core.thread;
+import std.parallelism;
+import std.concurrency;
+import std.range;
+
+void main() {
+    try {
+        int zero = 0;
+        // assert(zero != 0, "Can't be zero");
+        if (zero == 0) {
+            throw new Exception("Can't divide by zero");
+        } else {
+            int goodInt = 10 / zero;
+        }
+    } catch(Exception e) {
+        writeln(e.msg);
+    } finally {
+        writeln("Cleaning up");
+    }
+}
